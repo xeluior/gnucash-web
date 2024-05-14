a6af495acab084a81dc64a4b18015266ecfcf18e
Robert Gingras <developer@three-point-five.dev>
2024-05-14 18:12:26 -0400

    added session config options

--- src/gnucash_web/config/default.py
+++ src/gnucash_web/config/default.py
@@ -14,3 +14,12 @@ AUTH_MECHANISM = os.getenv('AUTH_MECHANISM')
 
 TRANSACTION_PAGE_LENGTH = int(os.getenv('TRANSACTION_PAGE_LENGTH', 25))
 PRESELECTED_CONTRA_ACCOUNT = os.getenv('PRESELECTED_CONTRA_ACCOUNT')
+
+SESSION_TYPE = os.getenv('SESSION_TYPE', 'redis')
+if SESSION_TYPE == 'redis':
+    import redis
+    SESSION_REDIS = redis.Redis(
+        host=os.getenv('REDIS_HOST', 'localhost'),
+        port=int(os.getenv('REDIS_PORT', 6379)),
+        password=os.getenv('REDIS_PASSWORD'),
+    )
